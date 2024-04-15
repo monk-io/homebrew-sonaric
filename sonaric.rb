1767d156deb2776b7af28bddbde371235e94c171
Circle CI job <cicd@sonaric.ai>
2024-04-15 09:17:29 +0000

    updated for v0.0.8

--- sonaric.rb
+++ sonaric.rb
@@ -1,12 +1,12 @@
 class Sonaric < Formula
   desc "Sonaric Network: the AI-powered backbone for all blockchains."
   homepage "https://sonaric.xyz"
-  version "0.0.7"
+  version "0.0.8"
 
-  url_x64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-darwin-v0.0.7.tar.gz"
-  sha256_x64 = "a6e1a8ffccf3bab58d9a39c334213a327a1a34217dea58f6bbebdb0a719075ea"
-  url_arm64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-arm-darwin-v0.0.7.tar.gz"
-  sha256_arm64 = "e62130577cc79aa476cdacd48aaf395afdaaa288e287f2a4796157569814a3f4"
+  url_x64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-darwin-v0.0.8.tar.gz"
+  sha256_x64 = "0eb0ca1f7adda36a71e198e6c825bb0cb20cffbd8d8960a0c1d3741768d4b5e1"
+  url_arm64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-arm-darwin-v0.0.8.tar.gz"
+  sha256_arm64 = "2b7567fecf225d0b3af209cca629ccc5d13684435260f3b69154f5a4cd41c95e"
 
   if Hardware::CPU.intel?
     sha256 sha256_x64
