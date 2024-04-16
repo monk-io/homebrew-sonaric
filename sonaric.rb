f394c530db0a53c5bc7334044e0c4a1f77a408de
Circle CI job <cicd@sonaric.ai>
2024-04-16 18:17:42 +0000

    updated for v0.0.11

--- sonaric.rb
+++ sonaric.rb
@@ -1,18 +1,12 @@
 class Sonaric < Formula
   desc "Sonaric Network: the AI-powered backbone for all blockchains."
   homepage "https://sonaric.xyz"
-  version "0.0.10"
+  version "0.0.11"
 
-  url_x64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-darwin-v0.0.10.tar.gz"
-  sha256_x64 = "9b47f24704022786c0288c4b84827730d32f2ed6483834184a0de6964f1ff0cf"
-  url_arm64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-arm-darwin-v0.0.10.tar.gz"
-  sha256_arm64 = "ce97a5bcdacb74887dfd34fc921e785722098b1800ead578603d90f8f4687eb9"
-
-  depends_on "podman" => :recommended
-
-  resource "sonaric-entrypoint" do
-    url "https://github.com/monk-io/homebrew-sonaric.git", branch: "main"
-  end
+  url_x64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-darwin-v0.0.11.tar.gz"
+  sha256_x64 = "1a586d718104fb2db34d237f8303586c569e4ccb617647dfe2d82c769ec31791"
+  url_arm64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-arm-darwin-v0.0.11.tar.gz"
+  sha256_arm64 = "d5b4c5fb61e9848453866a4404a162956ebb4421d3ce9ffd79cabe420eb9e574"
 
   if Hardware::CPU.intel?
     sha256 sha256_x64
@@ -22,25 +16,19 @@ class Sonaric < Formula
     url url_arm64
   end
 
+  depends_on "podman" => :recommended
+
   def install
-    resources.each do |r|
-      case r.name
-      when "sonaric-entrypoint"
-        bin.install r.cached_download/"sonaric-entrypoint.sh" => "sonaric-entrypoint"
-      end
-    end
     bin.install "sonaric" => "sonaric"
     bin.install "sonaricd" => "sonaricd"
   end
 
   def caveats; <<~EOS
-    Sonaric will be started automatically via brew services
-      brew services info sonaric
-    EOS
-  end
+    Initialize the sonaric machine with sonaric daemon inside
+      sonaric machine init
 
-  service do
-    run [opt_bin/"sonaric-entrypoint"]
-    keep_alive true
+    Upgrade sonaric daemon inside the sonaric machine to the latest version
+      sonaric machine upgrade
+    EOS
   end
 end
