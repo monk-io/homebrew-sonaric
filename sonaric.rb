e15e8247a9e368d7c5dc48388c753049e4ad6807
Circle CI job <cicd@sonaric.ai>
2024-04-23 08:34:11 +0000

    updated for v0.0.17

--- sonaric.rb
+++ sonaric.rb
@@ -1,12 +1,12 @@
 class Sonaric < Formula
   desc "Sonaric Network: the AI-powered backbone for all blockchains."
   homepage "https://sonaric.xyz"
-  version "0.0.16"
+  version "0.0.17"
 
-  url_x64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-darwin-v0.0.16.tar.gz"
-  sha256_x64 = "a6ee7c8c474b4721bbfc05fda9d293510ed72e8603e18177baf5785690b98532"
-  url_arm64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-arm-darwin-v0.0.16.tar.gz"
-  sha256_arm64 = "56307f31204d457fc6464093cf20b1b3c74c3f65c8edc7bbcd7eaf3e6dd70c20"
+  url_x64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-darwin-v0.0.17.tar.gz"
+  sha256_x64 = "593b9df7c114d20d46b04adba5c0dd9fe4ac3194155a915f9a7e41aa8cc425e7"
+  url_arm64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-arm-darwin-v0.0.17.tar.gz"
+  sha256_arm64 = "7521b1eeb507bcfebb16f702970eb3da6545b59322266a5ace0ca171580cf97b"
 
   depends_on "podman" => :recommended
 
