d83e67a690e650baaa45d664ca2bfed22f8e6e3e
Artem Teleshev <artem@monk.io>
2024-05-20 15:13:14 +0300

    scripts improvements

--- installer/install.sh
+++ installer/install.sh
@@ -6,6 +6,9 @@
 set -u
 
 NAME="Sonaric installer"
+SONARIC_OPTS="--nofancy --nocolor"
+BREW_SONARIC_PKG="monk-io/sonaric/sonaric"
+BREW_INSTALLER_LINK="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
 
 log() {
   echo "$@"
@@ -16,7 +19,7 @@ abort() {
   exit 1
 }
 
-addToPath(){
+add_to_path(){
   for arg in $@; do
     if [ -d "${arg}" ]; then
       if [[ "${PATH}" == "" ]]; then
@@ -34,6 +37,18 @@ addToPath(){
   done
 }
 
+# check_command {{name}} {{path}}
+check_command(){
+  local n="${1}"
+  local p="${2}"
+  if [[ "${p}" == "" ]]; then
+    abort "${n} not found"
+  fi
+  if [ ! -x "$(command -v ${p})" ]; then
+    abort "${p} not executable"
+  fi
+}
+
 # Fail fast with a concise message when not using bash
 # Single brackets are needed here for POSIX compatibility
 # shellcheck disable=SC2292
@@ -41,17 +56,26 @@ if [ -z "${BASH_VERSION:-}" ]; then
   abort "Bash is required to interpret this script."
 fi
 
-addToPath "/sbin" "/usr/sbin" "/usr/local/sbin"
-addToPath "/bin" "/usr/bin" "/usr/local/bin"
-addToPath "/opt/homebrew/bin"
+add_to_path "/sbin" "/usr/sbin" "/usr/local/sbin"
+add_to_path "/bin" "/usr/bin" "/usr/local/bin"
+add_to_path "/opt/homebrew/bin"
 
-TR="$(which tr 2>/dev/null)"
-ARCH="$(which arch 2>/dev/null)"
-EXPR="$(which expr 2>/dev/null)"
-UNAME="$(which uname 2>/dev/null)"
-SYSCTL="$(which sysctl 2>/dev/null)"
-DIRNAME="$(which dirname 2>/dev/null)"
-BASENAME="$(which basename 2>/dev/null)"
+TR=$(which tr 2>/dev/null)
+check_command "tr" "${TR}"
+CURL=$(which curl 2>/dev/null)
+check_command "curl" "${CURL}"
+ARCH=$(which arch 2>/dev/null)
+check_command "arch" "${ARCH}"
+EXPR=$(which expr 2>/dev/null)
+check_command "expr" "${EXPR}"
+UNAME=$(which uname 2>/dev/null)
+check_command "uname" "${UNAME}"
+SYSCTL=$(which sysctl 2>/dev/null)
+check_command "sysctl" "${SYSCTL}"
+DIRNAME=$(which dirname 2>/dev/null)
+check_command "dirname" "${DIRNAME}"
+BASENAME=$(which basename 2>/dev/null)
+check_command "basename" "${BASENAME}"
 
 OS="$(${UNAME} 2>/dev/null)"
 # Check if we are on mac
@@ -77,99 +101,48 @@ for p in $(echo ${PATH} | ${TR} ":" " "); do
   log " - ${p}"
 done
 
-USER_SHELL=$(${BASENAME} ${SHELL} 2>/dev/null)
-USER_SHELL_RC="~/.${USER_SHELL}rc"
-
-NCPU=$(${SYSCTL} -n hw.ncpu 2>/dev/null)
-MEMSIZE_MB=$(${EXPR} $(${SYSCTL} -n hw.memsize 2>/dev/null) / 1024 / 1024 2>/dev/null)
-log "${NAME} detects resources: NCPU=${NCPU} and MEMSIZE=${MEMSIZE_MB}(MB)"
-
-VM_OS_BUILD=""
-if [[ "${OS_ARCH}" == "arm64" ]]; then
-  # ISSUE: https://github.com/containers/podman/issues/22708
-  VM_OS_BUILD="39.20240407.3.0"
-fi
-
-PODMAN_MACHINE_CPUS=""
-PODMAN_MACHINE_MEMORY=""
-PODMAN_MACHINE_IMAGE=""
-
-if [ ${NCPU} -gt 0 ]; then
-  PODMAN_MACHINE_CPUS="--cpus ${NCPU}"
-fi
-
-if [ ${MEMSIZE_MB} -gt 0 ]; then
-  PODMAN_MACHINE_MEMORY="--memory ${MEMSIZE_MB}"
-fi
-
-if [[ "${VM_OS_BUILD}" != "" ]]; then
-  log "${NAME} uses build of VM OS: fedora coreos ${VM_OS_BUILD} applehv aarch64"
-  PODMAN_MACHINE_IMAGE="--image https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/${VM_OS_BUILD}/aarch64/fedora-coreos-${VM_OS_BUILD}-applehv.aarch64.raw.gz"
-fi
-
-HOMEBREW=$(which brew 2>/dev/null)
-if [ ! -x "$(command -v ${HOMEBREW})" ]; then
+BREW=$(which brew 2>/dev/null)
+if [ ! -x "$(command -v ${BREW})" ]; then
   log "Homebrew is not installed in your system, let's change it"
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  bash -c "$(${CURL} -fsSL ${BREW_INSTALLER_LINK})"
 
-  addToPath "/usr/local/bin" "/opt/homebrew/bin"
-  HOMEBREW=$(which brew 2>/dev/null)
-  if [ ! -x "$(command -v ${HOMEBREW})" ]; then
-    abort "Can not detect Homebrew, please follow the instruction on the screen and run this script again"
-  fi
+  add_to_path "/usr/local/bin" "/opt/homebrew/bin"
+  BREW=$(which brew 2>/dev/null)
+  check_command "brew" "${BREW}"
 fi
 
 log "Fetching the newest version of Homebrew and installed packages"
-${HOMEBREW} update
+${BREW} update
 
 log "Install the newest version of Sonaric"
-${HOMEBREW} install monk-io/sonaric/sonaric
+${BREW} install ${BREW_SONARIC_PKG}
 
 PODMAN=$(which podman 2>/dev/null)
-if [ ! -x "$(command -v ${PODMAN})" ]; then
-  abort "podman not found"
-fi
+check_command "podman" "${PODMAN}"
+SONARIC=$(which sonaric 2>/dev/null)
+check_command "sonaric" "${SONARIC}"
 
-log "Sonaric service stopping..."
-${HOMEBREW} services stop sonaric
+log "Sonaric service restarting..."
+${BREW} services restart sonaric
 
+ITR=0
 RUNNING=false
 while [[ "$RUNNING" != "true" ]]; do
-  case $(${PODMAN} machine inspect --format '{{.State}}' 2>/dev/null) in
-    stopped)
-      if [[ "${PODMAN_MACHINE_CPUS}" != "" ]]; then
-        log "Podman machine set ${PODMAN_MACHINE_CPUS}"
-        ${PODMAN} machine set ${PODMAN_MACHINE_CPUS}
-      fi
-      if [[ "${PODMAN_MACHINE_MEMORY}" != "" ]]; then
-        log "Podman machine set ${PODMAN_MACHINE_MEMORY}"
-        ${PODMAN} machine set ${PODMAN_MACHINE_MEMORY}
-      fi
-      # Start default podman machine
-      log "Podman machine starting..."
-      ${PODMAN} machine start || ${PODMAN} machine stop
-      ;;
-    running)
-      # Machine is already running
-      RUNNING=true
-      ;;
-    *)
-      # Initialize default podman machine
-      log "Podman machine initializing..."
-      ${PODMAN} machine init --now --rootful ${PODMAN_MACHINE_CPUS} ${PODMAN_MACHINE_MEMORY} ${PODMAN_MACHINE_IMAGE} || abort "Podman machine initializing failed"
-      ;;
-  esac
+  ITR=$((ITR + 1))
+  if ${SONARIC} ${SONARIC_OPTS} version; then
+    RUNNING=true
 
-  # Recheck machine each 3 sec
-  sleep 3
+    # If sonaric was started we should add some pause to become it ready
+    log "Wait for Sonaric daemon to become ready..."
+    sleep 15
+  else
+    log "${ITR}) Wait for Sonaric daemon to become started..."
+    sleep 5
+  fi
 done
 
-log "Check podman machines..."
+log "Check Podman machines..."
 ${PODMAN} machine ls
 
-log "Sonaric service starting..."
-${HOMEBREW} services start sonaric
-
-log "Wait for Sonaric service..."
-sleep 3
-${HOMEBREW} services info sonaric
+log "Check Sonaric service..."
+${BREW} services info sonaric
--- installer/uninstall.sh
+++ installer/uninstall.sh
@@ -6,9 +6,14 @@
 set -u
 
 NAME="Sonaric uninstaller"
+SONARIC_OPTS="--nofancy --nocolor"
 
 log() {
   echo "$@"
+}
+
+warn() {
+  echo "WARNING: $@"
 }
 
 abort() {
@@ -16,7 +21,7 @@ abort() {
   exit 1
 }
 
-addToPath(){
+add_to_path(){
   for arg in $@; do
     if [ -d "${arg}" ]; then
       if [[ "${PATH}" == "" ]]; then
@@ -34,6 +39,18 @@ addToPath(){
   done
 }
 
+# check_command {{name}} {{path}}
+check_command(){
+  local n="${1}"
+  local p="${2}"
+  if [[ "${p}" == "" ]]; then
+    abort "${n} not found"
+  fi
+  if [ ! -x "$(command -v ${p})" ]; then
+    abort "${p} not executable"
+  fi
+}
+
 # Fail fast with a concise message when not using bash
 # Single brackets are needed here for POSIX compatibility
 # shellcheck disable=SC2292
@@ -41,14 +58,18 @@ if [ -z "${BASH_VERSION:-}" ]; then
   abort "Bash is required to interpret this script."
 fi
 
-addToPath "/sbin" "/usr/sbin" "/usr/local/sbin"
-addToPath "/bin" "/usr/bin" "/usr/local/bin"
-addToPath "/opt/homebrew/bin"
+add_to_path "/sbin" "/usr/sbin" "/usr/local/sbin"
+add_to_path "/bin" "/usr/bin" "/usr/local/bin"
+add_to_path "/opt/homebrew/bin"
 
-RM="$(which rm 2>/dev/null)"
-TR="$(which tr 2>/dev/null)"
-UNAME="$(which uname 2>/dev/null)"
-BASENAME="$(which basename 2>/dev/null)"
+TR=$(which tr 2>/dev/null)
+check_command "tr" "${TR}"
+RM=$(which rm 2>/dev/null)
+check_command "rm" "${RM}"
+UNAME=$(which uname 2>/dev/null)
+check_command "uname" "${UNAME}"
+BASENAME=$(which basename 2>/dev/null)
+check_command "basename" "${BASENAME}"
 
 OS="$(${UNAME} 2>/dev/null)"
 # Check if we are on mac
@@ -62,12 +83,9 @@ for p in $(echo ${PATH} | ${TR} ":" " "); do
   log " - ${p}"
 done
 
-USER_SHELL=$(${BASENAME} ${SHELL} 2>/dev/null)
-USER_SHELL_RC="~/.${USER_SHELL}rc"
-
-HOMEBREW=$(which brew 2>/dev/null)
-if [ ! -x "$(command -v ${HOMEBREW})" ]; then
-  HOMEBREW=""
+BREW=$(which brew 2>/dev/null)
+if [ ! -x "$(command -v ${BREW})" ]; then
+  BREW=""
   log "Homebrew is not installed in your system"
 fi
 
@@ -77,27 +95,75 @@ if [ ! -x "$(command -v ${PODMAN})" ]; then
   log "Podman is not installed in your system"
 fi
 
-if [ "${HOMEBREW}" != "" ]; then
+SONARIC=$(which sonaric 2>/dev/null)
+if [ -x "$(command -v ${SONARIC})" ]; then
+  if [[ "${BREW}" != "" ]]; then
+    if ! "${SONARIC}" ${SONARIC_OPTS} "version"; then
+      log "Sonaric service starting..."
+      ${BREW} services start sonaric
+      log "Wait for Sonaric service to become ready..."
+      sleep 5
+    fi
+  fi
+
+  ITR=0
+  TOTAL_ITRS=50
+  RUNNING=false
+  while [[ "$RUNNING" != "true" && ${ITR} -le ${TOTAL_ITRS} ]]; do
+    ITR=$((ITR + 1))
+
+    if "${SONARIC}" ${SONARIC_OPTS} "version"; then
+      RUNNING=true
+
+      # If sonaric was started we should add some pause to become it ready
+      log "Wait for Sonaric daemon to become ready..."
+      sleep 15
+    else
+      log "${ITR}) Wait for Sonaric daemon to become started..."
+      sleep 5
+    fi
+  done
+
+  log "Sonaric resources stopping..."
+  if ! "${SONARIC}" ${SONARIC_OPTS} "stop" "-a"; then
+    warn "Sonaric resources stop failed"
+  fi
+
+  log "Sonaric resources deleting..."
+  if ! "${SONARIC}" ${SONARIC_OPTS} "delete" "-a" "--force"; then
+    warn "Sonaric resources delete failed"
+
+    if [[ "${PODMAN}" != "" ]]; then
+      if ! "${PODMAN}" "container" "prune" "-f"; then
+        warn "Podman containers force prune failed"
+      fi
+      if ! "${SONARIC}" ${SONARIC_OPTS} "delete" "-a" "--force"; then
+        warn "Sonaric resources cleanup failed"
+      fi
+    fi
+  fi
+fi
+
+if [ "${BREW}" != "" ]; then
   log "Sonaric service stopping..."
-  ${HOMEBREW} services stop sonaric
+  ${BREW} services stop sonaric
 fi
 
 if [ "${PODMAN}" != "" ]; then
   log "Podman machine stopping..."
   ${PODMAN} machine stop
-  sleep 3
 
   log "Podman machine removing..."
   ${PODMAN} machine rm -f
 fi
 
-if [ "${HOMEBREW}" != "" ]; then
+if [ "${BREW}" != "" ]; then
   log "Sonaric uninstalling..."
-  ${HOMEBREW} uninstall -f --ignore-dependencies sonaric
-
-  log "Podman uninstalling..."
-  ${HOMEBREW} uninstall -f --ignore-dependencies podman
+  ${BREW} uninstall -f --ignore-dependencies sonaric
 fi
 
-${RM} -rf ~/.local/share/containers
-${RM} -rf ~/.config/containers
+if [ ! -x "$(command -v $(which podman 2>/dev/null))" ]; then
+  log "Podman has already uninstalled from your system"
+  ${RM} -rf ~/.local/share/containers
+  ${RM} -rf ~/.config/containers
+fi
