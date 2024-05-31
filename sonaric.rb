e778f14bec4d2446434b392c380f57863ffa3011
Circle CI job <cicd@sonaric.ai>
2024-05-31 19:50:49 +0000

    updated for v1.0.0-core.1

--- sonaric.rb
+++ sonaric.rb
@@ -1,12 +1,12 @@
 class Sonaric < Formula
   desc "Sonaric Network: the AI-powered backbone for all blockchains."
   homepage "https://sonaric.xyz"
-  version "0.1.5"
+  version "1.0.0-core.1"
 
-  url_x64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-darwin-v0.1.5.tar.gz"
-  sha256_x64 = "4284528ab224d3f0ed7b186d8edd36879030bc5b85a36310197f51e8061139ef"
-  url_arm64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-arm-darwin-v0.1.5.tar.gz"
-  sha256_arm64 = "2e469a2454308f5a6f98ea98c071db116561ba18b7a0914838848dc5b1376d50"
+  url_x64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-darwin-v1.0.0-core.1.tar.gz"
+  sha256_x64 = "53db135c5e82df1031e61b359ce6ae0d374b00db762f953ff3edaf33a479c754"
+  url_arm64 = "https://storage.googleapis.com/sonaric-releases/stable/macos/sonaric-arm-darwin-v1.0.0-core.1.tar.gz"
+  sha256_arm64 = "a8259b06f11528d5ac9ee46221be8bd751a4619c9450a669c9668301fdd10027"
 
   depends_on "monk-io/sonaric/sonaric-runtime"
 
